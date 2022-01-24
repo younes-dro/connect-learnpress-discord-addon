2edb735aeea0bc16c61004e2738618e7d39db0d4
Ravi Soni <ravisoni6262@gmail.com>
2022-01-24 18:28:33 +0530

    Shree Ganesh

---
+++ admin/partials/learnpress-discord-addon-admin-display.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Provide a admin area view for the plugin
+ *
+ * This file is used to markup the admin-facing aspects of the plugin.
+ *
+ * @link       https://www.expresstechsoftwares.com
+ * @since      1.0.0
+ *
+ * @package    Learnpress_Discord_Addon
+ * @subpackage Learnpress_Discord_Addon/admin/partials
+ */
+?>
+
+<!-- This file should primarily consist of HTML with a little bit of PHP. -->
---
+++ includes/class-learnpress-discord-addon-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://www.expresstechsoftwares.com
+ * @since      1.0.0
+ *
+ * @package    Learnpress_Discord_Addon
+ * @subpackage Learnpress_Discord_Addon/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Learnpress_Discord_Addon
+ * @subpackage Learnpress_Discord_Addon/includes
+ * @author     ExpressTech Softwares Solutions Pvt Ltd <contact@expresstechsoftwares.com>
+ */
+class Learnpress_Discord_Addon_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
