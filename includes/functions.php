c18729fef16f378279ef4994cf16f566f4b695a8
younes-dro <dro66_8@hotmail.fr>
2022-03-09 07:53:26 +0100

    Fixe :  undefined function ets_learnpress_discord_as_get_action_data

--- includes/functions.php
+++ includes/functions.php
@@ -144,6 +144,21 @@ function ets_learnpress_discord_check_api_errors( $api_response ) {
 	}
 }
 /**
+ * Get Action data from table `actionscheduler_actions`
+ *
+ * @param INT $action_id
+ */
+function ets_learnpress_discord_as_get_action_data( $action_id ) {
+	global $wpdb;
+	$result = $wpdb->get_results( $wpdb->prepare( 'SELECT aa.hook, aa.status, aa.args, ag.slug AS as_group FROM ' . $wpdb->prefix . 'actionscheduler_actions as aa INNER JOIN ' . $wpdb->prefix . 'actionscheduler_groups as ag ON aa.group_id=ag.group_id WHERE `action_id`=%d AND ag.slug=%s', $action_id, LEARNPRESS_DISCORD_AS_GROUP_NAME ), ARRAY_A );
+        
+	if ( ! empty( $result ) ) {
+		return $result[0];
+	} else {
+		return false;
+	}
+}
+/**
  * Get the highest available last attempt schedule time
  */
 
