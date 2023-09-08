26624c9f51e1ef4a91a23b4f3b964ec8a19ebdc3
Younes DRO <dro66_8@hotmail.fr>
2023-09-08 21:29:17 +0100

    Update the review notice

--- includes/template/notification/review/review.php
+++ includes/template/notification/review/review.php
@@ -11,10 +11,17 @@ defined( 'ABSPATH' ) || exit;
 
 ?>
 <div class="notice notice-info is-dismissible ets-learnpress-discord-review-notice">
-<div class="ets-learnpress-discord-notice-icon"></div>
+
 <div class="ets-learnpress-discord-notice-content">
-	<p>If you have a few minutes, I would really appreciate it if you could leave a review of our support on :  <a href="https://wordpress.org/support/plugin/connect-learnpress-discord-add-on/reviews/">Leave a review</a></p>
-   <p>Reviews help us improve our support process and make it even better for future users.</p> 
+   <h1>Your <a href="https://wordpress.org/support/plugin/connect-learnpress-discord-add-on/">feedback</a> is invaluable!</h1>
+   <p>If you've found value in My <b>Connect LearnPress to Discord</b>, we'd greatly appreciate your support by leaving a <b><a href="https://wordpress.org/support/plugin/connect-learnpress-discord-add-on/">review &#x1F64F; on WordPress.</b> </a></p>
+   <p>It'll only take a couple of minutes and will go a long way in helping us reach more people.</p> 
+   <p class="ets-learnpress-discord-signature">
+	  <b>Ravi Soni</b>, Co-founder 
+	  <img src="<?php echo LEARNPRESS_DISCORD_PLUGIN_URL . 'admin/images/ravisoni.png'; ?>" />
+   </p>
+   <p>Please rate us: </p>
+   <p>&#x2B50; &#x2B50; &#x2B50; &#x2B50; &#x2B50;</p>
 </div>
 
 </div>
